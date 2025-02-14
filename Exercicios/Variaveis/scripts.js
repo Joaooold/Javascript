89aa1c8fe8613033b300cfba09b9cbcd8ba466bb
Joao Batista <joaofilhot09@gmail.com>
2025-02-14 14:02:36 -0300

    Estudando Variáveis

---
+++ Exercicios/Variaveis/scripts.js
@@ -0,0 +1,18 @@
+// tipo da variável + nome da variavel = informação da variável
+let mensagen = "Olá, Mundo"
+console.log(mensagen)
+
+let cor = "Vermelho" 
+console.log(cor)
+
+cor = "Azul"
+console.log(cor)
+
+cor = "Verde"
+console.log(cor)
+
+cor = "Preto"
+console.log(cor)
+
+cor = "Rosa"
+console.log(cor)
