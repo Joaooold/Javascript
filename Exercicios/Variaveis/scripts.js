132a0464999ed6260ae6b50caf9ba7b21a8f9bee
Joao Batista <joaofilhot09@gmail.com>
2025-02-15 11:37:11 -0300

    estudando variáveis

--- Exercicios/Variaveis/scripts.js
+++ Exercicios/Variaveis/scripts.js
@@ -1,14 +1,18 @@
+// TIPO LET
 // tipo da variável + nome da variavel = informação da variável
 let mensagen = "Olá, Mundo"
+console.log(mensagen)
+
+mensagen = "Oi, João"
 console.log(mensagen)
 
 let cor = "Vermelho" 
 console.log(cor)
 
-cor = "Azul"
+cor = "Azul" // Variáveis do tipo let podem ser mudadas de informação 
 console.log(cor)
 
-cor = "Verde"
+cor = "Verde" // Elas não podem ser repetidas let cor = "Azul"
 console.log(cor)
 
 cor = "Preto"
@@ -16,3 +20,24 @@ console.log(cor)
 
 cor = "Rosa"
 console.log(cor)
+
+// TIPO CONST
+
+const segundaMensagen = "Esta é uma variável do tipo const"
+console.log(segundaMensagen)
+
+// Variáveis do tipo const não podem ser mudadas de informaação e nao podem ser repetidas
+// segundaMensagen = "não funciona"
+
+// TIPO VAR
+nome = "vitor"
+console.log(nome)
+
+var nome = "João"
+console.log(nome)
+
+var nome = "Luiza"
+console.log(nome)
+
+nome = "lucelia"
+console.log(nome)
