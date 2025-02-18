798d29ab4b7a50bf1a4078a5f7ea9378bb6f7e17
Joao Batista <joaofilhot09@gmail.com>
2025-02-18 17:52:03 -0300

    estudando strings e numbers

---
+++ Exercicios/tiposPrimitivos/Strings-Numbers.js
@@ -0,0 +1,20 @@
+//String
+
+// "" é um texto - string normal
+// '' é uma mensagen - string normal
+// `` cria mais um texto - string template
+
+let mensagen = "Olá,";
+let nome = 'João';
+ let mensagen1 =`Olá, ${nome} Bem vindo ao curso`;
+
+//console.log(`${mensagen} ${nome}`+ " Bem vindo ao curso" )
+console.log(mensagen1)
+
+// Number 
+
+let numero = 50
+let numero1 = 1.23
+let decimal = 10
+
+console.log(numero / decimal)
