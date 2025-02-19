e9d071d57af4d071a9b6b3e2b3f23898ee820be1
Joao Batista <joaofilhot09@gmail.com>
2025-02-19 14:44:27 -0300

    estudando booleans

---
+++ Exercicios/tiposPrimitivos/Booleans.js
@@ -0,0 +1,11 @@
+let ligado = true // Verdadeiro - Sim = 1
+
+let desligado = false // Falso - Não = 0
+
+// console.log(true == 1)
+// console.log(false == 0)
+let nome = "pedro" ;
+console.log(Boolean(nome))
+
+let numero = 0
+console.log(Boolean(numero))
