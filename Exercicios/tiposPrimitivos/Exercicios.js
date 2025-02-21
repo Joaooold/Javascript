b969f06021e8070f65c4b2db78a5fbf255ab65c7
Joao Batista <joaofilhot09@gmail.com>
2025-02-21 19:08:58 -0300

    Exercicios de tipos de dados

---
+++ Exercicios/tiposPrimitivos/Exercicios.js
@@ -0,0 +1,44 @@
+// Pergunta 1 e 2
+let frase = "Fazendo Exercicios"
+console.log(typeof frase)
+console.log(frase)
+
+// Pergunta 3
+let nome = "João"
+let idade = 22
+let isStudent = true
+console.log(typeof nome)
+console.log(nome)
+console.log(typeof idade)
+console.log(idade)
+console.log(typeof isStudent)
+console.log(isStudent)
+
+// Pergunta 4
+nome = "Luiza"
+idade = 20
+isStudent = false
+console.log(nome)
+console.log(idade)
+console.log(isStudent)
+
+// Pergunta 5
+const numero = 30
+console.log(numero)
+
+// Pergunta 6
+let telefone
+let endereço
+console.log(telefone)
+console.log(endereço)
+
+// Pergunta 7
+let produto = "macarrao"
+let precoMacarrao = 8
+let estoqueMacarrao = 30
+
+// Pergunta 8
+let cidade = "São-paulo"
+let populacao = 11895578
+console.log(`A cidade de ${cidade} tem a população de ${populacao}`)
+
---
+++ Exercicios/tiposPrimitivos/Undefined-Null.js
@@ -0,0 +1,10 @@
+// Undefined
+let carro
+let moto = undefined
+console.log(typeof carro)
+console.log(typeof moto)
+
+// null
+let poste = null
+console.log(poste)
+console.log(typeof poste)
