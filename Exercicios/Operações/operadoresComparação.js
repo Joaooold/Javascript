610997afbfd85eef7a1d1683e2cd96eb9602b614
Joao Batista <joaofilhot09@gmail.com>
2025-02-25 14:41:58 -0300

    estudando comparações

---
+++ Exercicios/Operações/operadoresComparação.js
@@ -0,0 +1,10 @@
+// Igualdade == / estrita igualdade === / Tipo e valor igual
+console.log(5 == "5")
+console.log(5 === "5") // Melhor utilizado
+
+// Desigualdade != / estrita desigualdade !== / Tipo e valor igual
+console.log(5 != "5")
+console.log(5 !== "5") // Melhor utilizado
+
+// maior que >
+console.log(5 > 5)
--- Exercicios/Operações/operaçõesAritmetricas.js
+++ Exercicios/Operações/operaçõesAritmetricas.js
@@ -16,3 +16,24 @@ console.log(numero1 * numero2)
 
 console.log(numero2 / numero1)
 
+// Módulo - resto da divisão
+
+let divisao = 9 / 5
+let modulo = 9 % 5
+console.log(divisao)
+console.log(modulo) 
+
+// Exponêciação - 2 elevado a 5 = 32
+
+let numero = 2
+let expoente = 5
+let resultado = numero ** expoente
+console.log(resultado)
+
+// Incremento ++ (Adiciona mais 1) ou decremeto -- (Diminui em 1) 
+
+let controle = 0
+console.log(++controle)
+console.log(++controle)
+console.log(--controle)
+console.log(++controle)
