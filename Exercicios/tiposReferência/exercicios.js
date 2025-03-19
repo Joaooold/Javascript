c5ffaef505d8f38e3a6c3351a81416dfeb4c8f46
Joao Batista <joaofilhot09@gmail.com>
2025-03-19 13:03:51 -0300

    Praticando Array e object

---
+++ Exercicios/tiposReferência/exercicios.js
@@ -0,0 +1,24 @@
+// Pergunta 1 Array
+let Produto = ["Placa de video", "Mouse", "PC"]
+console.log(Produto[1])
+Produto[2] = "Notebook"
+console.log(Produto)
+
+// Pergunta 2 Array
+let Clientes = ["João", "Luiza", "Lucelia"]
+Clientes[3] = "Batista"
+Clientes.length = Clientes.length - 1
+console.log(Clientes)
+
+// Pergunta 3 Object
+let ProdutoOBJ = {
+    nome: "Macarrão",
+    preco: 15,
+    Quantidade: 10,
+}
+// console.log(ProdutoOBJ.nome)
+ ProdutoOBJ.preco = 25
+console.log(ProdutoOBJ)
+
+
+
