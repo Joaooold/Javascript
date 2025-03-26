0a02af00e23ea43181f437786238f8807d782bc1
Joao Batista <joaofilhot09@gmail.com>
2025-03-26 14:52:07 -0300

    continuando funcoes

--- Exercicios/funcoes/funcoes.js
+++ Exercicios/funcoes/funcoes.js
@@ -1,25 +1,40 @@
-function enviarMensagen() {
-    console.log("Bem vindo ao nosso site por favor se cadraste")
-}
-// enviarMensagen()
+// function enviarMensagen() {
+//     console.log("Bem vindo ao nosso site por favor se cadraste")
+// }
+// // enviarMensagen()
 
-function cadrastar(nome, sobrenome) {
-    console.log(`muito obrigado ${nome} ${sobrenome} por se cadrastar`)
-}
+// function cadrastar(nome, sobrenome) {
+//     console.log(`muito obrigado ${nome} ${sobrenome} por se cadrastar`)
+// }
 
-// cadrastar("João", "Batista")
+// // cadrastar("João", "Batista")
 
-function banco(deposito, saque) {
-    let saldo = deposito - saque
-    return saldo
-    // console.log(`O seu saldo no banco é de ${saldo}`)
-}
-// saldo(500, 600)
+// function banco(deposito, saque) {
+//     let saldo = deposito - saque
+//     return saldo
+//     // console.log(`O seu saldo no banco é de ${saldo}`)
+// }
+// // saldo(500, 600)
 
-function main() {
-    enviarMensagen()
-    cadrastar("João", "Batista")
-    let saldo = banco(500, 100)
-    console.log(`O seu saldo é de ${saldo}`)
+// function main() {
+//     enviarMensagen()
+//     cadrastar("João", "Batista")
+//     let saldo = banco(500, 100)
+//     console.log(`O seu saldo é de ${saldo}`)
+// }
+// main()
+
+// Chamando funções
+
+function trabalhadores(nome, profissao) {
+    console.log(`O trabalhador: ${nome} e seu trabalho: ${profissao}`)
 }
-main()
+trabalhadores("João", "Engenheiro")
+trabalhadores("Lucelia", "Tutora")
+trabalhadores("Luiza", "Fisioterapeuta")
+trabalhadores("Lucas", "Grendene")
+
+function mensagem() {
+    console.log("imprimindo a mensagem")
+}
+mensagem()
