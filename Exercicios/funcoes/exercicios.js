a18798cca694fd868ea74ac642568e9ea60c7227
Joao Batista <joaofilhot09@gmail.com>
2025-04-15 10:11:41 -0300

    fazendo exercicios sobre função

---
+++ Exercicios/funcoes/exercicios.js
@@ -0,0 +1,14 @@
+// Pergunta 01
+function notas(N1,N2,N3) {
+    return alunoNota = N1 + N2 + N3 
+
+}
+notas(6,7,9)
+console.log(`A média desse aluno é ${alunoNota}`)
+
+function mediaFinal(N4) {
+    return notaFinal = notas(6,7,9) + N4/2
+
+}
+mediaFinal(5)
+console.log(`a nota final desse aluno é ${notaFinal}`)
