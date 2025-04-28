5cbf28a3c5fb51f7f893dfa6e90759a60135391e
Joao Batista <joaofilhot09@gmail.com>
2025-04-28 15:27:45 -0300

    estudando métodos em strings

---
+++ Exercicios/Métodos/metodosStrings.js
@@ -0,0 +1,13 @@
+// Metodos strings 
+
+let string = "Olá, Mundo!"
+
+console.log(string.toUpperCase()) // converte para letras maiusculas
+console.log(string.toLowerCase()) // converte para letras minusculas
+
+// slice(start, end(opcional))
+console.log(string.slice(0, 6))
+console.log(string.slice(-6)) // inverte de tras para fente
+
+// substring(start, end(opcional))
+console.log(string.substring(1))
