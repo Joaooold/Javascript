a306070295bf1f910c1ed23dd4a26972e44930e5
Joao Batista <joaofilhot09@gmail.com>
2025-05-06 17:55:26 -0300

    estudando metodos arrays

---
+++ Exercicios/Métodos/metodosArrays.js
@@ -0,0 +1,13 @@
+let produtos = ["Bola", "Carrinho", "Boneco"]
+
+produtos.push("video game") // adiciona ao final da lista
+// console.log(produtos)
+
+produtos.unshift("Cesta") // adiciona ao inicio da lista
+console.log(produtos)
+
+produtos.shift()// Retira o primeiro iten da lista
+console.log(produtos) 
+
+produtos.pop() // Retira o ultimo iten da lista
+console.log(produtos)
