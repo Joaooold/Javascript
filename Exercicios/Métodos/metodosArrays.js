a35f1be468190c7ac60de4e684c4fc3aa1a4679f
Joao Batista <joaofilhot09@gmail.com>
2025-05-19 18:05:18 -0300

    continuando metodos arrays

--- Exercicios/Métodos/metodosArrays.js
+++ Exercicios/Métodos/metodosArrays.js
@@ -11,3 +11,23 @@ console.log(produtos)
 
 produtos.pop() // Retira o ultimo iten da lista
 console.log(produtos)
+
+// Exemplo 02
+
+let letras = ["f", "h", "d", "e", "c", "a", 'i',"b","g"]
+let numeros = [2, 4, 6, 20, 10, 3, 12, 7, 1]
+
+letras.sort() // deixa as coisas de forma ordenada
+console.log(letras)
+
+numeros.sort() // Qaundo for numeros ordena de forma diferente
+console.log(numeros)
+
+letras.reverse() // Inverte as coisas
+console.log(letras)
+
+numeros.reverse()
+console.log(numeros)
+
+let concatenaçãoArray = letras.concat(numeros) // Faz a concatenação das variaveis
+console.log(concatenaçãoArray)
