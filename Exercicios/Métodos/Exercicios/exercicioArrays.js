cf262f81d074903fab296eda7d63e5066e30ac18
Joao Batista <joaofilhot09@gmail.com>
2025-05-23 14:09:55 -0300

    exercicios sobre metodos arrays

---
+++ Exercicios/Métodos/Exercicios/exercicioArrays.js
@@ -0,0 +1,21 @@
+// Pergunta 01
+let produtos01 = ["bola", "video game"]
+produtos01.shift()
+produtos01.push("Computador")
+console.log(produtos01)
+
+// Pergunta 02
+let produtos02 = ["Janela", "Mesa"]
+produtos02.push("Panela")
+let junçaoProdutos = produtos02.concat(produtos01)
+console.log(junçaoProdutos)
+
+// Pergunta 03
+let produtos03 = ["lápis", "Caderno", "Estojo"]
+console.log(produtos03.includes("lápis"))
+console.log(produtos03.includes("agenda"))
+
+// Pergunta 04
+let numeros = [2, 5, 8, 1]
+
+// Pergunta 05
