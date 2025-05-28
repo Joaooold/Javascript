20b4c5c91166f149ddb70d4b7a6cf491750a159d
Joao Batista <joaofilhot09@gmail.com>
2025-05-28 13:32:20 -0300

    metodos em objetos

---
+++ Exercicios/Métodos/metodosobj.js
@@ -0,0 +1,9 @@
+let produtos = {
+    nome: "Laptop",
+    preço: 2500,
+    disponibilidade: true,
+}
+
+console.log(Object.keys(produtos)) // propriedades do objeto em lista
+console.log(Object.values(produtos)) // valor da propriedade de objeto em lista
+console.log(Object.entries(produtos)) // lista contendo lista de propriedades e valores
