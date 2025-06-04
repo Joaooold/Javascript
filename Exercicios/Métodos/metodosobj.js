beed811563dee6d616314e7e30c14bc5b708b1ff
Joao Batista <joaofilhot09@gmail.com>
2025-06-04 13:47:52 -0300

    terminando object methods

--- Exercicios/Métodos/metodosobj.js
+++ Exercicios/Métodos/metodosobj.js
@@ -12,7 +12,7 @@ console.log(Object.entries(produtos)) // lista contendo lista de propriedades e 
 Object.assign(produtos, {emEstoque: 20}) // Adicone uma propriedade e valor
 console.log(produtos)
 
-Object.defineProperty(produtos, "disponibilidade", {value: false}) // Muda uma propriedade de valor
+Object.defineProperty(produtos, "disponibilidade", {value: false}) // Muda o valor de uma propriedade
 console.log(produtos)
 
 const origem = {
@@ -34,4 +34,29 @@ Object.defineProperty(destino, "salario", { // configuração para acrescentar u
     writable: true,
     configurable: true
 })
+ delete destino.cidade // deleta uma propriedade
 console.log(destino)
+
+// Aula 03
+
+const pessoa = {
+    nome: "João",
+    surf: true,
+    falar: function () { console.log("Olá") },
+}
+const carro = {
+    modelo: "Corolla",
+    marca: "Toyota"
+
+}
+const joao = Object.create(pessoa) // herda as propriedades de um objeto
+Object.assign(joao, carro) // Cópia as propriedades para outro objeto
+joao.falar()
+joao.nome = "João Batista" // Adicionando propriedades para o objeto joao
+joao.idade = 22
+joao.cidade = "EUA"
+console.log(joao.surf, joao.nome)
+
+console.log(joao)
+console.log(Object.hasOwn(joao, "nome")) // Verifica se a propriedade existe
+
