6fa81241c70ecfe1922acf941a6f45622fbc0438
Joao Batista <joaofilhot09@gmail.com>
2025-06-11 16:51:00 -0300

    continuando estudos

--- Exercicios/Métodos/Exercicios/exerciciosObj.js
+++ Exercicios/Métodos/Exercicios/exerciciosObj.js
@@ -12,6 +12,17 @@ const venda = {
 console.log(venda.CalcularTotal())
 
 // Pergunta 02
+venda.aplicarDesconto = function(percentual) {
+    this.desconto = percentual
+}
+venda.aplicarDesconto(20)
+console.log(`O desconto foi de ${venda.desconto}%, venda foi de ${venda.CalcularTotal()}`)
+
 // Pergunta 03
+venda.mundarQuantidade = function(novaQuantidade) {
+    this.quantidade = novaQuantidade
+}
+venda.mundarQuantidade(6)
+console.log(`A quantidade foi de ${venda.quantidade}, venda foi de ${venda.CalcularTotal()}`)
 // Pergunta 04
 // Pergunta 05
