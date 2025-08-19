ab4b9fa6b3f7fe0b4384ce684d168430829f0555
Joao Batista <joaofilhot09@gmail.com>
2025-08-19 17:56:48 -0300

    objetos globais

---
+++ Exercicios/Métodos/objetosGlobais.js
@@ -0,0 +1,31 @@
+// Math Aula 01
+let pi = Math.PI
+console.log(pi)
+
+let raizQuadrada = Math.sqrt(100)
+console.log(raizQuadrada)
+
+let exponenciação = Math.pow(2,5) // base, elevado
+console.log(exponenciação)
+
+// Aula 02
+let numero = 3.34
+let numero2 = 3.55
+console.log(Math.round(numero)) //Arredonda o numero para o valor mais próximo
+console.log(Math.round(numero2))
+
+let aléatorio = Math.random() * 100 // digita um valor pseudo-aleatorio entre 0 e 1 (podendo ser modificado como no exemplo * 100)
+console.log(Number.parseFloat(aléatorio.toFixed()))
+
+// Aula 03
+// Math.min() - retorna o valor minimo encontrado dentro de uma lista de números
+// Math.max() - retorna o valor maximo encontrado dentro de uma lista de números
+
+console.log(Math.min(8,44,56,67,22,5))
+console.log(Math.max(8,44,56,67,22,5))
+
+// console.log(Math.max([8,44,56,67,22,5])) não funciona retorna NaN
+let lista = [8,44,56,67,22,5]
+ console.log(Math.max(lista)) // tambem retorna NaN
+console.log(Math.max(...lista)) // spread operador ... (espalha espalha informações da lista)
+console.log(Math.min(...lista))
