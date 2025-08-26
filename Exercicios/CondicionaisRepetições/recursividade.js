2f9d075b348c1faf972085d7cfe074aecf918282
Joao Batista <joaofilhot09@gmail.com>
2025-08-26 15:43:59 -0300

    ...

--- Exercicios/CondicionaisRepetições/recursividade.js
+++ Exercicios/CondicionaisRepetições/recursividade.js
@@ -16,4 +16,40 @@ function recursividade(string) {
   }
   return 1 + recursividade(string.substring(1));
 }
-console.log(recursividade(""));
+// console.log(recursividade(""));
+
+// recursividade Object
+const carro = {
+  marca: "Toyota",
+  modelo: "Corolla",
+  ano: 2024,
+  peças: {
+    portas: 4,
+    airbag: true,
+    pneus: "sistema abs",
+  },
+};
+// Função recursiva para imprimir propriedades de um objeto simples
+function imprimirObjeto(objeto) {
+  // Obter as chaves do objeto
+  const keys = Object.keys(objeto); // lista [marca,modelo,carro]
+  // Caso o objeto estiver vazio
+  if (keys.length === 0) {
+    return; //interronper a função
+  }
+  //   interação sobre as chaves do objeto
+  for (let index = 0; index < keys.length; index++) {
+    const key = keys[index];
+    const value = objeto[key];
+    // verificação se o valor é um objeto
+    if (typeof value === "object") {
+      //Se for objeto chama recursivamente a função
+      imprimirObjeto(value);
+    } else {
+      // Se não for objeto, imprimi chave e seu valor
+      console.log(`${key}: ${value}`);
+    }
+  }
+}
+imprimirObjeto(carro);
+
