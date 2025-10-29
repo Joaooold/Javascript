bd48ab51923560e41bf1d1ed4ed99f567867e4aa
Joao Batista <joaofilhot09@gmail.com>
2025-10-29 12:10:11 -0300

    validação de formulario

--- Aulas Html-JavaScript/Dom/formularios/validaçãoDeFormularios/aula02/script.js
+++ Aulas Html-JavaScript/Dom/formularios/validaçãoDeFormularios/aula02/script.js
@@ -2,23 +2,52 @@ document.addEventListener("DOMContentLoaded", function () {
   const formulario = document.getElementById("meuFormulario");
 
   formulario.addEventListener("submit", function (event) {
+    resetarMensagensError();
+
     if (!validarCamposObrigatorios()) {
       event.preventDefault(); // impede a submissão se houver erros
     }
-
-    function validarCamposObrigatorios() {
-      let camposObrigatorios = document.querySelectorAll(".obrigatorio");
-      let camposValidos = true;
-
-      for (let i = 0; camposObrigatorios.length > i; i++) {
-        let campo = camposObrigatorios[i];
-        if (campo.value === "" || campo.value === null) {
-          console.log("Este campo é obrigatório");
-          camposValidos = false;
-        }
-      }
-      // console.log(camposValidos)
-      return camposValidos;
+    if (!campoSenha()) {
+      event.preventDefault();
     }
   });
+
+  function validarCamposObrigatorios() {
+    let camposObrigatorios = document.querySelectorAll(".obrigatorio");
+    let camposValidos = true;
+
+    for (let i = 0; camposObrigatorios.length > i; i++) {
+      let campo = camposObrigatorios[i];
+      if (campo.value === "" || campo.value === null) {
+        exibirError(campo, "Este campo é obrigatório");
+        camposValidos = false;
+      }
+    }
+    // console.log(camposValidos)
+    return camposValidos;
+  }
+
+  function campoSenha() {
+    const senha = document.getElementById("senha");
+    const confirmarSenha = document.getElementById("confirmarSenha");
+
+    if (senha.value !== confirmarSenha.value) {
+      exibirError(confirmarSenha, "As senhas não coincidem");
+      return false;
+    }
+    return true;
+  }
+
+  function exibirError(elemento, mensagem) {
+    let mensagemError = elemento.parentElement.querySelector(".error-message");
+    mensagemError.textContent = mensagem;
+    mensagemError.style.display = "inline-block";
+  }
+
+  function resetarMensagensError() {
+    let mensagensError = document.querySelectorAll(".error-message");
+    for (let i = 0; i < mensagensError.length; i++) {
+      mensagensError[i].textContent = "";
+    }
+  }
 });
