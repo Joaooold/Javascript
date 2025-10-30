55ba51c3cbee37bdb49e795a17014c8900820e83
Joao Batista <joaofilhot09@gmail.com>
2025-10-30 17:45:47 -0300

    Calculadora IMC

---
+++ Aulas Html-JavaScript/Dom/formularios/validaçãoDeFormularios/aula03/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const formulario = document.getElementById("meuFormulario")
+
+    formulario.addEventListener("submit", function(e) {
+        e.preventDefault()
+        let dados = new FormData(formulario)
+        for(let [chave, valor] of dados.entries()) {
+            console.log(chave + ": " + valor)
+        }
+    })
+})
