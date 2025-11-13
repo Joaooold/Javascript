4a55e0417a065800767fac664c2edb6ad07648cd
Joao Batista <joaofilhot09@gmail.com>
2025-11-13 17:53:43 -0300

    estudando npm

---
+++ Aulas JavaScript/Backend/Aula 1/script.js
@@ -0,0 +1,8 @@
+// CLI
+
+// cd === caminho de diretorio para navegar no node
+// ls para saber quantas pastas ou arquivos tem 
+
+// Aula de npm === node package manager
+
+console.log("Utilizando CLI - npm ")
