fe3ab6915c9f5f9f6ab0d4a83f79c2616477beaa
Joao Batista <joaofilhot09@gmail.com>
2025-11-20 13:29:43 -0300

    começando api

--- Aulas JavaScript/Backend/Aula 1/script.js
+++ Aulas JavaScript/Backend/Aula 1/script.js
@@ -1,8 +1,10 @@
-// CLI
+// CLI - linhas de comando no terminal
 
 // cd === caminho de diretorio para navegar no node
 // ls para saber quantas pastas ou arquivos tem 
 
 // Aula de npm === node package manager
+
+// Utilizei o "jest para testa o instalamento"
 
 console.log("Utilizando CLI - npm ")
