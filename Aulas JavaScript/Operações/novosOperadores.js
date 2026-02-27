22289cf6a3b59b38579df288be3b6db0add3b625
Joao Batista <joaofilhot09@gmail.com>
2026-02-27 13:35:19 -0300

    continuandi novos operadores

--- Aulas JavaScript/Operações/novosOperadores.js
+++ Aulas JavaScript/Operações/novosOperadores.js
@@ -26,7 +26,7 @@ const participantes = numeroDeParticipantes ?? 0;
 // Junta parâmetros de um funcao
 function sum(...parcela) {
   return parcela.reduce((acc, cureentValue) => {
-    acc + cureentValue, 0
+    (acc + cureentValue, 0);
   });
 }
 console.log(sum(2, 4, 6, 8, 9, 10));
@@ -35,16 +35,40 @@ console.log(sum(2, 4, 6, 8, 9, 10));
 // Ele permite que você copie ou modifique arrays e objetos de maneira fácil e eficiente.
 const objectA = {
   name: "joao",
-  sobrenome:"batista",
-  id: 1
+  sobrenome: "batista",
+  id: 1,
+};
+const listA = [2, 33, 555, 1, 0, 45];
+
+const listB = [...listA];
+const objectB = { ...objectA, saldoBancario: "1_000_000_000" };
+listB[2] = 1000;
+
+// console.log(listA);
+// console.log(listB);
+// console.log(objectA);
+// console.log(objectB);
+
+// Destructuring
+const person = {
+  personName: "joao",
+  lastName: "Batista",
+  age: 23,
+  netWorth: 1_000_000,
+};
+// formas de usar Destructuring
+// const { personName, lastName, age, netWorth } = person;
+// console.log(`A pessoa ${personName} ${lastName} com a idade de ${age} tem o saldo bancario de ${netWorth}`);
+
+function print({ personName, lastName, age, netWorth }) {
+  console.log(`A pessoa ${personName} ${lastName} com a idade de ${age} tem o saldo bancario de ${netWorth}`);
 }
-const listA = [2, 33, 555, 1 ,0 ,45]
+print(person);
 
-const listB = [...listA]
-const objectB = {...objectA, saldoBancario: "1_000_000_000"}
-listB[2] = 1000
-
-console.log(listA)
-console.log(listB)
-console.log(objectA)
-console.log(objectB)
+// Com array
+const semestreNotas = [8, 7, 9, 4]
+const [primeiraNota, segundaNota, ...rest] = semestreNotas
+// Usando o rest para juntar as notas 
+console.log(primeiraNota)
+console.log(segundaNota)
+console.log(...rest)
