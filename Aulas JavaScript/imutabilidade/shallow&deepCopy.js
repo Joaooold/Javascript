1784e7dfdd09b6232e333cea99681a56a1ff24fc
Joao Batista <joaofilhot09@gmail.com>
2026-03-03 15:10:08 -0300

    imutabilidade

---
+++ Aulas JavaScript/imutabilidade/shallow&deepCopy.js
@@ -0,0 +1,18 @@
+// shallow copy - copia superficial
+// Objeto original
+const produto = {
+  nome: "Caderno",
+  preco: 15.5,
+  categoria: "Papelaria",
+};
+// Objeto cópia
+const produto2 = { ...produto };
+produto2.nome = "Agenda"
+console.log('Objeto original:', produto)
+console.log('Objeto copia:', produto2)
+
+// Deep copy - copia profunda
+const produto3 = JSON.parse(JSON.stringify(produto))
+console.log('copia profunda do produto', produto3)
+produto3.preco = 50
+console.log('copia profunda do produto', produto3)
